e7ed89785e8a2110c6a5c68139ba4410f43d67e3
Taylor Yamashita <taylory@princeton.edu>
2023-11-02 23:25:37 -0400

    Initial word2vec experimenting

--- mecab.py
+++ mecab.py
@@ -3,12 +3,15 @@ import MeCab
 import demoji
 import re
 from stop_words import stop_words
+import gensim, logging
+
+# cleaning and tokenizing
 
 mt = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
-
 file = open('test-tweets.txt', 'r')
 
 # we'll just test on a few tweets for now
+tweets = []
 for i in range(40):
     # load tweet, convert to py dict, access content
     tweet_json = file.readline()
@@ -29,4 +32,13 @@ for i in range(40):
         components.append(parsed.surface)
         parsed = parsed.next
     components = [token for token in components if not token in stop_words]
-    print(components, "\n")
+    tweets.append(components)
+
+print(tweets)
+
+# word2vec
+
+# set up logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+model = gensim.models.Word2Vec(tweets, min_count=5)
