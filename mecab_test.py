a1f9b9fd61803d4b0cca24e23a98bf1d92dfbb24
Taylor Yamashita <taylory@princeton.edu>
2023-12-26 16:15:15 -0800

    update test file

--- mecab_test.py
+++ mecab_test.py
@@ -1,8 +1,10 @@
 import json
 import MeCab
 import demoji
+import mojimoji
 import re
-from stop_words import stop_words
+from stopwords_ja import stop_words
+from stopwords_slothlib import stop_words_2
 import gensim, logging
 
 # cleaning and tokenizing
@@ -16,27 +18,47 @@ for i in range(100):
     # load tweet, convert to py dict, access content
     tweet_json = file.readline()
     tweet_py = json.loads(tweet_json)
-    tweet_text = tweet_py['rawContent'] # note: need other prop for over 140 char?
+    text = tweet_py['rawContent'] # note: need other prop for over 140 char?
+
+    # print(text)
 
     # clean tweet content
-    remove_emojis = demoji.replace(tweet_text, "")
-    remove_more_emojis = re.sub("([\uD83E-\uD83E])+", "", remove_emojis)
-    remove_newlines = re.sub("(\n)+", "", remove_more_emojis)
-    remove_usernames = re.sub("@([a-zA-Z0-9_]+)", "", remove_newlines)
-    remove_hashtags = re.sub("#([a-zA-Z0-9_ぁ-んァ-ン一-龠]+)", "", remove_usernames)
-    remove_links = re.sub("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", remove_hashtags)
-    remove_punc = re.sub("([-.,;\"\'!?~@#$%^&*():\{\}\[\]\/\\\\]+)", "", remove_links)
-    remove_jp_punc = re.sub("([\uFF01-\uFF0F\uFF1A-\uFF20\uFF3B-\uFF40\uFF5B-\uFF65\uFF9E-\uFFEE\u3000-\u303F]+)", "", remove_punc)
-    remove_geo_shapes = re.sub("([\u25A0-\u25FF])+", "", remove_jp_punc)
-    remove_misc_symbols = re.sub("([\u2600-\u26FF])+", "", remove_geo_shapes)
+    # from https://colab.research.google.com/drive/1bX-JyY4xmCm_RFkJg3QNcthUvEJaBghP
+    # handle half-width/full-width chars, jp punctuation
+    text = text.lower()
+    text = mojimoji.zen_to_han(text, kana=False)
+    text = mojimoji.han_to_zen(text, digit=False, ascii=False)
+    text = text.translate(str.maketrans({
+        '!': '！', '"': '”', '#': '＃', '$': '＄', '%': '％', '&': '＆', '\'': '’',
+        '(': '（', ')': '）', '*': '＊', '+': '＋', ',': '，', '-': '−', '.': '．',
+        '/': '／', ':': '：', ';': '；', '<': '＜', '=': '＝', '>': '＞', '?': '？',
+        '@': '＠', '[': '［', '\\': '＼', ']': '］', '^': '＾', '_': '＿', '`': '｀',
+        '{': '｛', '|': '｜', '}': '｝'
+        }))
+    zenkaku_leftsingle = b'\xe2\x80\x98'.decode('utf-8')
+    text = re.sub('[’´｀]', zenkaku_leftsingle, text)
+    
+    # remove twitter-specific strings (handles, hashtags, etc.)
+    text = re.sub("@([a-zA-Z0-9_]+)", "", text)
+    text = re.sub("#([a-zA-Z0-9_ぁ-んァ-ン一-龠]+)", "", text)
+    text = re.sub("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", text)
+
+    # remove emojis
+    text = demoji.replace(text, "")
+    text = re.sub("([\uD83E-\uD83E])+", "", text)
+
+    # remove punctuation and whitespace
+    text = re.sub("([^一-龯ぁ-んァ-ン])+","",text)  
+    text = re.sub("(\s)+", "", text)
 
     # mecab tokenization
-    parsed = mt.parseToNode(remove_misc_symbols)
+    parsed = mt.parseToNode(text)
     components = []
     while parsed:
         components.append(parsed.surface)
         parsed = parsed.next
-    components = [token for token in components if not token in stop_words]
+    # components = [token for token in components if ((not token in stop_words) and (not token in stop_words_2))]
+    print(components)
     tweets.append(components)
 
 # word2vec
@@ -49,4 +71,8 @@ model = gensim.models.Word2Vec(tweets, min_count=3)
 
 # check similarity given by trained model
 sim = model.wv.most_similar('今日')
-print(sim)
+# print(sim)
+
+top_50 = model.wv.index_to_key[:50]
+# for word in top_50:
+#     print(word)
